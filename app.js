3b52a35ad41cb49f42770b43659a3d879543782d
Brayan Hernandez <brayanbehc.com>
2025-01-20 13:03:36 -0600

    comienzo de la programacion

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres= [];
+
+
+function agregarAmigo(){
+    let ingresoAmigo = document.getElementById(amigo);
+
+    if (ingresoAmigo=""){
+        console. log = ("Debes ingresar un nombre valido");
+    }
+}
